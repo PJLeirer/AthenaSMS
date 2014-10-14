8f4503326f7e2e65d628d72734da46c112256c5c
PJ Leirer <pjleirer@gmail.com>
2014-10-14 00:12:32 -0500

    scheduled jobs

---
+++ scheduled/RunMonthlyScheduledJobs/RunMonthlyScheduledJobs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunMonthlyScheduledJobs
+{
+    class Program
+    {
+        // host address and port;
+        public static IPAddress hostIP = new IPAddress(new byte[] { 192, 168, 1, 71 }); // remote
+        //public static IPAddress hostIP = IPAddress.Any; // local
+        public static int hostPort = 11420;
+
+
+        public static SyncUser mSyncUser;
+
+        public static SockMan mSockMan;
+
+        public static bool isRunning = true;
+
+
+
+        static void Main(string[] args)
+        {
+
+            mSyncUser = new SyncUser();
+
+            mSockMan = new SockMan();
+
+
+
+
+        }
+    }
+}
---
+++ scheduled/RunWeeklyScheduledJobs/RunWeeklyScheduledJobs/SyncUser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunWeeklyScheduledJobs
+{
+    public class SyncUser
+    {
+        private int userID = 0;
+        private String userName = null;
+        private int userLevel = 0;
+        private bool loggedIn = false;
+        public bool isLoggedIn()
+        {
+            if (loggedIn && userID > 0 && userLevel > 0 && userName != null)
+            {
+                return true;
+            }
+            else
+            {
+                if (!loggedIn)
+                {
+                    Console.WriteLine("loggedIn is false");
+                }
+                if (userID < 1)
+                {
+                    Console.WriteLine("userID less than 1");
+                }
+                if (userLevel < 1)
+                {
+                    Console.WriteLine("userLevel less than 1");
+                }
+                if (userName == null)
+                {
+                    Console.WriteLine("userName is null");
+                }
+                return false;
+            }
+        }
+        public int getUserID()
+        {
+            return userID;
+        }
+        public String getUserName()
+        {
+            return userName;
+        }
+        public int getUserLevel()
+        {
+            return userLevel;
+        }
+
+        //used by sockrecv
+        public void assignUserInfo(int ID, string Name, int Level)
+        {
+            Console.WriteLine("assigning local user info");
+            this.userID = ID;
+            this.userName = Name;
+            this.userLevel = Level;
+            if (ID > 0)
+            {
+                loggedIn = true;
+            }
+        }
+    }
+}
