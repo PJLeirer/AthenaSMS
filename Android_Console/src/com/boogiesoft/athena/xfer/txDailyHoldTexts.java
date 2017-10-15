002161c94ef40aa2fb0e1fb696f622876960e1d0
PJ Leirer <pjleirer@gmail.com>
2017-10-15 15:56:59 -0500

    Android folder

--- Android_Console/src/com/boogiesoft/athena/xfer/txDailyHoldTexts.java
+++
@@ -1,9 +0,0 @@
-
-package com.boogiesoft.athena.xfer;
-
-import java.io.Serializable;
-
-public class txDailyHoldTexts implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public boolean runHolds = false;;
-}
